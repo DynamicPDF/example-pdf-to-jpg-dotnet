bc0d0961601468444c2f04e23670074612d8555a
Andrew Cochran <andrew@dynamicpdf.com>
2019-08-21 10:12:09 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using ceTe.DynamicPDF.Rasterizer;
+using System;
+using System.Text.RegularExpressions;
+
+namespace example_pdf_to_jpg_dotnet
+{
+    // This example shows how to convert a PDF document to JPEG/JPG.
+    // It references the ceTe.DynamicPDF.Rasterizer.NET NuGet package.
+    class Program
+    {
+        // Convert a PDF document to 4 JPEG files (one for each page).
+        // Use the ceTe.DynamicPDF.Rasterizer namespace for the PdfRasterizer class.
+        static void Main(string[] args)
+        {
+            PdfRasterizer rasterizer = new PdfRasterizer(GetResourcePath("doc-a.pdf"));
+
+            rasterizer.Draw("output.jpg", ImageFormat.Jpeg, ImageSize.Dpi72);
+        }
+
+        // This is a helper function to get the full path to a file in the Resources folder.
+        public static string GetResourcePath(string inputFileName)
+        {
+            var exePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+            Regex appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+            var appRoot = appPathMatcher.Match(exePath).Value;
+            return System.IO.Path.Combine(appRoot, "Resources", inputFileName);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+﻿# Convert PDF to JPEG/JPG Example for C# (.NET Core)
+
+This example shows how to convert a PDF document to JPEG/JPG. More details can be found
+[here](https://www.dynamicpdf.com/examples/pdf-to-jpg-.net-core).
